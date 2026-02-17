68c349c814f58d987e459f756dda7da9d035f0a9
SparklingByte <118135751+SparklingByte@users.noreply.github.com>
2026-02-17 21:37:53 +0100

    fix: use the useRouter hook provided by next-intl

--- src/components/Projects/projects-showcase.tsx
+++ src/components/Projects/projects-showcase.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { motion } from 'framer-motion'
-import { useRouter } from 'next/navigation'
+import { useRouter } from '@/i18n/navigation'
 import { useTranslations } from 'next-intl'
 import type React from 'react'
 import { memo, useCallback, useMemo } from 'react'
